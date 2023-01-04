be62e3c998e547db172ba1f12353673419b1c14b
Poletansky Viktor <poletansky@gmail.com>
2023-01-04 16:41:36 +0300

    init

---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"github.com/hashicorp/terraform-plugin-sdk/v2/helper/schema"
+	"github.com/hashicorp/terraform-plugin-sdk/v2/plugin"
+
+	"github.com/rprtr258/seventv-tf-provider/seventv"
+)
+
+func main() {
+	plugin.Serve(&plugin.ServeOpts{
+		ProviderFunc: func() *schema.Provider {
+			return seventv.Provider()
+		},
+	})
+}
