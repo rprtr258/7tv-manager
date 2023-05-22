844e0090fc2620b1c5e43e51a817f3e8889b68b5
Poletansky Viktor <poletansky@gmail.com>
2023-05-22 19:02:03 +0300

    no-tf: u

--- Makefile
+++ Makefile
@@ -1,2 +1,8 @@
-run:
-	@go run cmd/7tv/main.go push --token "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiNjNiNTgwMmFlYmZiYzJkYzRkZjI4ODQ3IiwidiI6MCwiaXNzIjoic2V2ZW50di1hcGkiLCJleHAiOjE2OTI1NDMwOTYsIm5iZiI6MTY4NDc2NzA5NiwiaWF0IjoxNjg0NzY3MDk2fQ.DCW35XYMV9LvsV_0YWvofQjJsaQ28Qijw7qeePLVjT8" --config examples/testtest2.json
+TOKEN := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1IjoiNjNiNTgwMmFlYmZiYzJkYzRkZjI4ODQ3IiwidiI6MCwiaXNzIjoic2V2ZW50di1hcGkiLCJleHAiOjE2OTI1NDMwOTYsIm5iZiI6MTY4NDc2NzA5NiwiaWF0IjoxNjg0NzY3MDk2fQ.DCW35XYMV9LvsV_0YWvofQjJsaQ28Qijw7qeePLVjT8"
+7TV := go run cmd/7tv/main.go
+
+push:
+	@${7TV} push --token ${TOKEN} --config examples/push.jsonnet
+
+pull:
+	@${7TV} pull --token ${TOKEN} --config examples/pull.jsonnet --id "63b58083c032521d3d256191"
--- cmd/7tv/main.go
+++ cmd/7tv/main.go
@@ -31,82 +31,144 @@ func mapDiff(old, new map[string]string) (toRemove, toAdd map[string]string) {
 	return
 }
 
-func main() {
-	if err := (&cli.App{
+type EmotesetConfig struct {
+	Name   string            `json:"name"`
+	Emotes map[string]string `json:"emotes"`
+}
+
+type Config map[string]EmotesetConfig
+
+var (
+	_cmdPull = &cli.Command{
+		Name:  "pull",
+		Usage: "pull emotes state from 7tv to config",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "token",
+				Usage: "auth token, get from requests to gql api on 7tv.app",
+			},
+			&cli.StringFlag{
+				Name:      "config",
+				Usage:     "config file to write to",
+				TakesFile: true,
+			},
+			&cli.StringFlag{
+				Name:  "id",
+				Usage: "emote set id to pull (for now it cannot pull all emotesets)",
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			token := ctx.String("token")
+			configPath := ctx.String("config")
+			emotesetID := ctx.String("id")
+
+			client, err := api.NewClient(token)
+			if err != nil {
+				return fmt.Errorf("new client: %w", err)
+			}
+
+			actualEmoteset, err := client.EmoteSet().Read(emotesetID)
+			if err != nil {
+				return fmt.Errorf("get setID=%s: %w", emotesetID, err)
+			}
+
+			config := Config{
+				emotesetID: EmotesetConfig{
+					Name:   actualEmoteset.Name,
+					Emotes: actualEmoteset.Emotes,
+				},
+			}
+			configBytes, err := json.MarshalIndent(config, "", "  ")
+			if err != nil {
+				return fmt.Errorf("marshal config: %w", err)
+			}
+
+			if err := os.WriteFile(configPath, configBytes, 0o644); err != nil {
+				return fmt.Errorf("read config %s: %w", configPath, err)
+			}
+
+			return nil
+
+		},
+	}
+	_cmdPush = &cli.Command{
+		Name:  "push",
+		Usage: "push emotes state from config to 7tv",
+		Flags: []cli.Flag{
+			&cli.StringFlag{
+				Name:  "token",
+				Usage: "auth token, get from requests to gql api on 7tv.app",
+			},
+			&cli.StringFlag{
+				Name:      "config",
+				Usage:     "config file with emotesets descriptions",
+				TakesFile: true,
+			},
+		},
+		Action: func(ctx *cli.Context) error {
+			token := ctx.String("token")
+			configPath := ctx.String("config")
+
+			configBytes, err := os.ReadFile(configPath)
+			if err != nil {
+				return fmt.Errorf("read config %s: %w", configPath, err)
+			}
+
+			var config Config
+			if err := json.Unmarshal(configBytes, &config); err != nil {
+				return fmt.Errorf("parse config: %w", err)
+			}
+
+			client, err := api.NewClient(token)
+			if err != nil {
+				return fmt.Errorf("new client: %w", err)
+			}
+
+			for emotesetID, emoteset := range config {
+				actualEmoteset, err := client.EmoteSet().Read(emotesetID)
+				if err != nil {
+					return fmt.Errorf("get setID=%s: %w", emotesetID, err)
+				}
+
+				toRemove, toAdd := mapDiff(actualEmoteset.Emotes, emoteset.Emotes)
+
+				for name, emoteID := range toRemove {
+					log.Infof("removing emote", log.F{"name": name, "emoteID": emoteID})
+					if err := client.Emote().RemoveFromSet(emotesetID, emoteID); err != nil {
+						return fmt.Errorf("remove emoteID=%q name=%q: %w", emoteID, name, err)
+					}
+				}
+
+				for name, emoteID := range toAdd {
+					log.Infof("adding emote", log.F{"name": name, "emoteID": emoteID})
+					if err := client.Emote().AddToSet(emotesetID, emoteID, name); err != nil {
+						return fmt.Errorf(
+							"add emoteID=%s name=%s to setID=%s: %w",
+							emoteID, name, emotesetID, err,
+						)
+					}
+				}
+			}
+
+			return nil
+
+		},
+	}
+	_app = &cli.App{
 		Name:            "7tv",
 		Usage:           "cli utility to manage emote sets",
 		HideHelp:        true,
 		HideHelpCommand: true,
 		HideVersion:     true,
 		Commands: []*cli.Command{
-			{
-				Name:  "push",
-				Usage: "push emotes state from config to 7tv",
-				Flags: []cli.Flag{
-					&cli.StringFlag{
-						Name:  "token",
-						Usage: "auth token, get from requests to gql api on 7tv.app",
-					},
-					&cli.StringFlag{
-						Name:      "config",
-						Usage:     "config file with emotesets descriptions",
-						TakesFile: true,
-					},
-				},
-				Action: func(ctx *cli.Context) error {
-					token := ctx.String("token")
-					configPath := ctx.String("config")
-
-					configBytes, err := os.ReadFile(configPath)
-					if err != nil {
-						return fmt.Errorf("read config %s: %w", configPath, err)
-					}
-
-					var config map[string]struct {
-						Name   string            `json:"name"`
-						Emotes map[string]string `json:"emotes"`
-					}
-					if err := json.Unmarshal(configBytes, &config); err != nil {
-						return fmt.Errorf("parse config: %w", err)
-					}
-
-					client, err := api.NewClient(token)
-					if err != nil {
-						return fmt.Errorf("new client: %w", err)
-					}
-
-					for emotesetID, emoteset := range config {
-						actualEmoteset, err := client.EmoteSet().Read(emotesetID)
-						if err != nil {
-							return fmt.Errorf("get setID=%s: %w", emotesetID, err)
-						}
-
-						toRemove, toAdd := mapDiff(actualEmoteset.Emotes, emoteset.Emotes)
-
-						for name, emoteID := range toRemove {
-							log.Infof("removing emote", log.F{"name": name, "emoteID": emoteID})
-							if err := client.Emote().RemoveFromSet(emotesetID, emoteID); err != nil {
-								return fmt.Errorf("remove emoteID=%q name=%q: %w", emoteID, name, err)
-							}
-						}
-
-						for name, emoteID := range toAdd {
-							log.Infof("adding emote", log.F{"name": name, "emoteID": emoteID})
-							if err := client.Emote().AddToSet(emotesetID, emoteID, name); err != nil {
-								return fmt.Errorf(
-									"add emoteID=%s name=%s to setID=%s: %w",
-									emoteID, name, emotesetID, err,
-								)
-							}
-						}
-					}
-
-					return nil
-
-				},
-			},
+			_cmdPush,
+			_cmdPull,
 		},
-	}).Run(os.Args); err != nil {
+	}
+)
+
+func main() {
+	if err := _app.Run(os.Args); err != nil {
 		log.Fatal(err.Error())
 	}
 }
